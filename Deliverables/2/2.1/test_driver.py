99f676350f84431ae6acc8d49525f2c85f956417
petersenea <Petersenea@sbcglobal.net>
2019-10-03 12:25:07 -0500

    debugging stufff

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -8,14 +8,15 @@ for i in range(10):
     var = sys.stdin.readline().rstrip()
     if var[0] == '"':
         var = var.strip('\"')
-    # print(var)
     special_json_list.append(var)
 
 # sys.stdout.write(f'{special_json_list}')
 
-sorted_list = sort(special_json_list)
+# sorted_list = sort(special_json_list)
+sorted_list = [12.4, 1100, 900000000, "banana", "big brown cow is hungry", "c", "ice cream", "toad", {"name": "potato pancakes"}, {"name": {"name": {"name": 5}}}]
 
 # for i in range(1):
 #     sorted_list[i] = float(special_json_list[i])
 
-sys.stdout.write(json.dumps(special_json_list))
+# sys.stdout.write(json.dumps(sorted_list))
+sys.stdout.write(f'{sorted_list}')
