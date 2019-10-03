6b17e4a1845425df12e82e21d61dfaeb518972de
petersenea <Petersenea@sbcglobal.net>
2019-10-03 12:29:56 -0500

    changed test driver

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -12,11 +12,11 @@ for i in range(10):
 
 # sys.stdout.write(f'{special_json_list}')
 
-# sorted_list = sort(special_json_list)
-sorted_list = [12.4, 1100, 900000000, "banana", "big brown cow is hungry", "c", "ice cream", "toad", {"name": "potato pancakes"}, {"name": {"name": {"name": 5}}}]
+sorted_list = sort(special_json_list)
+# sorted_list = [12.4, 1100, 900000000, "banana", "big brown cow is hungry", "c", "ice cream", "toad", {"name": "potato pancakes"}, {"name": {"name": {"name": 5}}}]
 
 # for i in range(1):
 #     sorted_list[i] = float(special_json_list[i])
 
 # sys.stdout.write(json.dumps(sorted_list))
-sys.stdout.write(f'{sorted_list}')
+json.dump(sorted_list, sys.stdout)
