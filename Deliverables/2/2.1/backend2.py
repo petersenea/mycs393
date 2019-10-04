4333af9b1ed9ad1c786ccfd727f2345dc05991eb
Julia <juliabrain2020@u.northwestern.edu>
2019-10-03 21:02:55 -0500

    clean code

--- Deliverables/2/2.1/backend2.py
+++ Deliverables/2/2.1/backend2.py
@@ -1,29 +1,17 @@
 import json
 import functools
 
+
+
+
 """
     sorts and formats special_json_list
 """
 def sort(special_json_list):
-
-    _insertion_sort(special_json_list)
-
-    _format_list(special_json_list)
-
-    return special_json_list
-
-"""
-    ########### private helper functions ##########
-"""
-
-"""
-    Insertion Sorts special_json_list in place in ascending order
-"""
-def _insertion_sort(special_json_list):
-    
+    special_json_list[0] = _type(special_json_list[0])
     for i in range(1,len(special_json_list)):
 
-        value = special_json_list[i]
+        value = _type(special_json_list[i])
 
         j = i - 1
 
@@ -33,21 +21,34 @@ def _insertion_sort(special_json_list):
 
         special_json_list[j+1] = value
 
-
+    return special_json_list
 
 """
-    Returns True if a is greater than b, False otherwise, used recursively on json objects
+    ########### private helper functions ##########
 """
+
+
+def sortOrder(item):
+    if isinstance(item, float) or isinstance(item, int):
+        return 1
+    if isinstance(item, str):
+        return 2
+    if isinstance(item, dict):
+        return 3
+    else:
+        print(type(item))
+
+
 def _is_greater(a, b):
 
     # determine the types of a and b, and convert them to their types
-    type_a, a = _type(a)
-    type_b, b = _type(b)
+    order_a = sortOrder(a)
+    order_b = sortOrder(b)
 
-    if type_a == type_b:
+    if order_a == order_b:
 
         # both a and b are jsons, so run _is_greater on their "name" values recursively
-        if type_a == 3:
+        if order_a == 3:
             return _is_greater(a["name"], b["name"])
         
         # both a and b are either strings or numbers, use python's > function
@@ -56,28 +57,25 @@ def _is_greater(a, b):
 
     # a and b are different types and thus follow type hierarchy of json > string > number
     else:
-        return type_a > type_b
-
-
+        return order_a > order_b
 
 """
-    Returns the type of the input 
-        (one of 1 (for numbers), 2 (for strings), or 3 (for jsons)) 
-    AND the input converted from string or dict to its type 
-        (one of float, string, or json)
+    Returns True if a is greater than b, False otherwise, used recursively on json objects
 """
+
 def _type(thing):
 
     # check if thing is a dict, thus a json
     if isinstance(thing, dict):
-        return 3, thing
-
+        return thing
+    elif thing.isdigit():
+        return int(thing)
     else:
 
         # check if string thing is a float, thus a number
         try:
             float(thing)
-            return 1, float(thing)
+            return float(thing)
 
         except (TypeError, ValueError):
 
@@ -85,31 +83,32 @@ def _type(thing):
             try:
                 json_obj = json.loads(thing)
 
-                return 3, json_obj
+                return json_obj
             
             except ValueError:
 
                 # check if string thing is a string, not strictly necessary but is useful for clear code
                 if type(thing) == str:
-                    return 2, thing
+                    return thing
                 else:
                     print("not a string, json, or float")
 
-
-
 """
-    Returns True if a string of type "number" is an int
-""" 
-def _is_int(number):
-    a = float(number)
-    b = int(a)
-    return a == b
+    Returns the type of the input 
+        (one of 1 (for numbers), 2 (for strings), or 3 (for jsons)) 
+    AND the input converted from string or dict to its type 
+        (one of float, string, or json)
+"""
+
+
+
 
 
     
 """
     Converts all of the elements of special_json_list into their appropriate types
 """
+'''
 def _format_list(special_json_list):
     for i in range(len(special_json_list)):
         thing = special_json_list[i]
@@ -129,7 +128,7 @@ def _format_list(special_json_list):
             special_json_list[i] = thing
 
 
-
+'''
 
 
 
@@ -197,4 +196,90 @@ def _type(thing):
                     return "string", thing
                 else:
                     print("not a string, json, or float")
+'''
+
+
+'''
+
+"""
+    Insertion Sorts special_json_list in place in ascending order
+"""
+def _insertion_sort(special_json_list):
+    for i in range(1,len(special_json_list)):
+
+        value = _type(special_json_list[i])
+        print(value)
+
+        j = i - 1
+
+        while j >= 0 and _is_greater(special_json_list[j], value):
+            special_json_list[j+1] = special_json_list[j]
+            j -= 1
+
+        special_json_list[j+1] = value
+'''
+
+'''
+def _is_greater(a, b):
+
+    # determine the types of a and b, and convert them to their types
+    type_a, a = _type(a)
+    type_b, b = _type(b)
+
+    if type_a == type_b:
+
+        # both a and b are jsons, so run _is_greater on their "name" values recursively
+        if type_a == 3:
+            return _is_greater(a["name"], b["name"])
+        
+        # both a and b are either strings or numbers, use python's > function
+        else:
+            return a > b
+
+    # a and b are different types and thus follow type hierarchy of json > string > number
+    else:
+        return type_a > type_b
+'''
+
+
+'''
+def _type(thing):
+
+    # check if thing is a dict, thus a json
+    if isinstance(thing, dict):
+        return 3, thing
+
+    else:
+
+        # check if string thing is a float, thus a number
+        try:
+            float(thing)
+            return 1, float(thing)
+
+        except (TypeError, ValueError):
+
+            # check if string thing is a json object, thus a json
+            try:
+                json_obj = json.loads(thing)
+
+                return 3, json_obj
+            
+            except ValueError:
+
+                # check if string thing is a string, not strictly necessary but is useful for clear code
+                if type(thing) == str:
+                    return 2, thing
+                else:
+                    print("not a string, json, or float")
+'''
+
+
+"""
+    Returns True if a string of type "number" is an int
+""" 
+'''
+def _is_int(number):
+    a = float(number)
+    b = int(a)
+    return a == b
 '''
