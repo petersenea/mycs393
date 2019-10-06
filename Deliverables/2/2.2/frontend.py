96ed04812a94bfaefffb2d0b0841747856188687
petersenea <Petersenea@sbcglobal.net>
2019-10-06 17:43:10 -0500

    Update frontend.py

--- Deliverables/2/2.2/frontend.py
+++ Deliverables/2/2.2/frontend.py
@@ -33,10 +33,14 @@ sorted_list = json.dumps(list_sorted_lists)
 sys.stdout.write(sorted_list)
 """
 
+"""
 
-
+# holds all the objects read from stdin
 original_json_obj_list = []
+
+# holds beginning parts of a json
 temp_json = ""
+
 
 for line in sys.stdin:
     line = line.rstrip()
@@ -55,8 +59,40 @@ for line in sys.stdin:
 
 num_full_lists = floor(len(original_json_obj_list) / 10)
 split_json_lists = [original_json_obj_list[x:x+10] for x in range(0,num_full_lists*10, 10)]
-# for x in chunks:
-    # print(x)
+
+list_sorted_lists = [sort(x) for x in split_json_lists]
+
+sorted_list = json.dumps(list_sorted_lists)
+sys.stdout.write(sorted_list)
+
+"""
+
+
+# holds all the objects read from stdin
+original_json_obj_list = []
+
+# holds beginning parts of a json
+temp_json = ""
+
+
+for line in sys.stdin:
+    line = line.rstrip()
+
+    # line_words = line.split()
+
+    for char in line:
+
+        temp_json += char
+        try:
+            obj = json.loads(temp_json)
+            original_json_obj_list.append(obj)
+            temp_json = ""
+        except:
+            pass 
+
+num_full_lists = floor(len(original_json_obj_list) / 10)
+split_json_lists = [original_json_obj_list[x:x+10] for x in range(0,num_full_lists*10, 10)]
+
 list_sorted_lists = [sort(x) for x in split_json_lists]
 
 sorted_list = json.dumps(list_sorted_lists)
