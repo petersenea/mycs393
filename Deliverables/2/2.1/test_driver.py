8643db62c546c000296176c8c97ab2d61a4f6c8c
petersenea <Petersenea@sbcglobal.net>
2019-10-05 20:24:00 -0500

    maybe fixed it

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -1,24 +1,39 @@
 import sys
 from backend2 import sort
 import json
-# import ijson
 
-"""
+
 special_json_list = []
 
 
 i = 0
+temp = ""
 
 while i < 10:
     # remove newline character after each special json obj
     var = sys.stdin.readline().rstrip()
 
     if var != "":
-        special_json_list.append(var)
-        i += 1
+        try:
+            json.loads(var)
+            special_json_list.append(var)
+            i+=1
+        except:
+            temp += var
+            try:
+                json.loads(temp)
+                special_json_list.append(temp)
+                i+=1
+                temp = ""
+                # print("made a valid json")
+            except:
+                # print("haven't made a json yet")
+                pass
+            # print("not a valid json")
+    
+sorted_list = sort(special_json_list)
+
 """
-
-
 special_json_list = []
 # for i in range(10):
     
@@ -26,6 +41,7 @@ data = json.load(sys.stdin)
 special_json_list.append(obj)
 
 sorted_list = sort(special_json_list)
+"""
 
 
 """
