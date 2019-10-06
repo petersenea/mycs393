162cfb3c85742117c95cbfc25126ebac9127bd35
petersenea <Petersenea@sbcglobal.net>
2019-10-06 12:43:15 -0500

    maybe worked

--- Deliverables/2/2.2/frontend.py
+++ Deliverables/2/2.2/frontend.py
@@ -1,12 +1,19 @@
 import sys
 import json
 from backend import sort
+from math import floor
 
 original_json_obj_list = []
 temp_json = ""
+num_objs = 0
+
+# list_sorted_lists = []
+
+# stuff = sys.stdin.read()
 
 for line in sys.stdin:
     line = line.rstrip()
+    # print(line)
     if line != "":
 
         temp_json += line
@@ -18,7 +25,15 @@ for line in sys.stdin:
         except:
             pass
 
-sorted_list = original_json_obj_list
+chunks = [original_json_obj_list[x:x+10] for x in range(0,floor(len(original_json_obj_list)/10)*10, 10)]
+# for x in chunks:
+    # print(x)
+list_sorted_lists = [sort(x) for x in chunks]
 
-sorted_list = json.dumps(sorted_list)
+
+# sorted_list = original_json_obj_list
+
+
+
+sorted_list = json.dumps(list_sorted_lists)
 sys.stdout.write(sorted_list)
