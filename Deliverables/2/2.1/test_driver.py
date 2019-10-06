dbc7dbfb4e2ba189bbd959489e1944dd03d29cd1
petersenea <Petersenea@sbcglobal.net>
2019-10-05 20:02:15 -0500

    changes

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -1,17 +1,39 @@
 import sys
 from backend2 import sort
 import json
+# import ijson
 
+"""
 special_json_list = []
 
-for i in range(10):
+
+i = 0
+
+while i < 10:
     # remove newline character after each special json obj
     var = sys.stdin.readline().rstrip()
-   
-    special_json_list.append(var)
+
+    if var != "":
+        special_json_list.append(var)
+        i += 1
+"""
+
+
+special_json_list = []
+# for i in range(10):
+    
+data = json.load(sys.stdin)
+special_json_list.append(obj)
 
 sorted_list = sort(special_json_list)
 
-# print(type(sorted_list))
-# json.dump(sorted_list, sys.stdout,ensure_ascii=False)
+
+"""
+for event in ijson.parse(sys.stdin):
+    print(event)
+    special_json_list.append(event)
+
+sorted_list = sort(special_json_list)
+"""
+
 sys.stdout.write(sorted_list)
