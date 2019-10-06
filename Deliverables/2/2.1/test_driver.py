284cb8bb1af0075ef119d9556aedaae468205c99
petersenea <Petersenea@sbcglobal.net>
2019-10-05 20:03:14 -0500

    Update test_driver.py

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -4,11 +4,24 @@ import json
 
 special_json_list = []
 
+
+i = 0
+
+while i < 10:
+    # remove newline character after each special json obj
+    var = sys.stdin.readline().rstrip()
+
+    if var != "":
+        special_json_list.append(var)
+        i += 1
+
+"""
 for i in range(10):
     # remove newline character after each special json obj
     var = sys.stdin.readline().rstrip()
    
     special_json_list.append(var)
+"""
 
 sorted_list = sort(special_json_list)
 
