266f6f7ebd79c0ba5a569c153c297bb1a5d5b88b
petersenea <Petersenea@sbcglobal.net>
2019-10-06 20:05:06 -0500

    blah

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -2,33 +2,6 @@ import sys
 import json
 from backend import sort
 
-"""
-special_json_list = []
-
-num_objs = 0
-temp_json = ""
-
-while num_objs < 10:
-    # remove newline character after each special json obj
-    var = sys.stdin.readline().rstrip()
-    
-    if var != "":
-
-        temp_json += var
-        try:
-            obj = json.loads(temp_json)
-            special_json_list.append(obj)
-            num_objs += 1
-            temp_json = ""
-        except:
-            pass
-
-sorted_list = sort(special_json_list)
-sorted_list = json.dumps(sorted_list)
-
-sys.stdout.write(sorted_list)
-"""
-
 special_json_list = []
 
 num_objs = 0
