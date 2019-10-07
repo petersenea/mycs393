5dc4e3ce6fe44952fedfc71d88eca1eec14bdebc
Julia <juliabrain2020@u.northwestern.edu>
2019-10-06 21:56:28 -0500

    fixed json loading

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -2,56 +2,31 @@ import sys
 import json
 from backend import sort
 
-"""
-special_json_list = []
 
-num_objs = 0
-temp_json = ""
-
-while num_objs < 10:
-    # remove newline character after each special json obj
-    var = sys.stdin.readline().rstrip()
-    
-    if var != "":
-
-        temp_json += var
-        try:
-            obj = json.loads(temp_json)
-            special_json_list.append(obj)
-            num_objs += 1
-            temp_json = ""
-        except:
-            pass
-
-sorted_list = sort(special_json_list)
-sorted_list = json.dumps(sorted_list)
-
-sys.stdout.write(sorted_list)
-"""
 
 special_json_list = []
 
 num_objs = 0
-temp_json = ""
+str_json = ""
+decoder = json.JSONDecoder()
 
-while num_objs < 10:
-    # remove newline character after each special json obj
-    line = sys.stdin.readline().rstrip()
+
+for line in sys.stdin:
+    str_json += line
+
+while len(str_json)>0 or num_objs<10:
+    try:
+        obj, idx = decoder.raw_decode(str_json)
+        str_json = str_json[idx:]
+        special_json_list.append(obj)
+        num_objs += 1
+    except:
+        str_json = str_json[1:]
     
-    line_words = line.split()
 
-    for words in line_words:
 
-        temp_json += " " + words
-        try:
-            obj = json.loads(temp_json)
-            special_json_list.append(obj)
-            num_objs += 1
-            temp_json = ""
-        except:
-            pass
+if num_objs == 10:
+    sorted_list = sort(special_json_list)
+    sorted_list = json.dumps(sorted_list)
 
-sorted_list = sort(special_json_list)
-sorted_list = json.dumps(sorted_list)
-
-sys.stdout.write(sorted_list)
+    sys.stdout.write(sorted_list)
