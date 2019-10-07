3ae74746cb87eefc16c01f3e10fe8fa12421eb4c
petersenea <Petersenea@sbcglobal.net>
2019-10-06 19:57:21 -0500

    Update test_driver.py

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -2,6 +2,7 @@ import sys
 import json
 from backend import sort
 
+"""
 special_json_list = []
 
 num_objs = 0
@@ -28,3 +29,29 @@ sorted_list = sort(special_json_list)
 sorted_list = json.dumps(sorted_list)
 
 sys.stdout.write(sorted_list)
+"""
+
+special_json_list = []
+
+num_objs = 0
+temp_json_obj = ""
+temp_json_str = ""
+temp_json_num = ""
+
+
+while num_objs < 10:
+
+    line = sys.stdin.readline().rstrip()
+    
+    for char in line:
+
+        if char == '"' and temp_json_obj == "" and temp_json_str = "" and temp_json_num = "":
+            # start creating a string
+            temp_json_str += char
+        elif char == '"' and temp_json_str != "":
+            # finish creating a string
+            temp_json_str += char
+        elif char == "{":
+
+        elif
+
