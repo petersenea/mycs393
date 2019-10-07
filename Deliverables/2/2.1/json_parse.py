165734b12bf1d80fae42d81e5b4eaf5b53e47151
petersenea <Petersenea@sbcglobal.net>
2019-10-06 19:56:54 -0500

    starting to change stuff

---
+++ Deliverables/2/2.1/json_parse.py
@@ -0,0 +1,6 @@
+import json
+
+with open('input0') as file:
+    data = json.load(file)
+
+print(data)
--- Deliverables/2/2.2/frontend.py
+++ Deliverables/2/2.2/frontend.py
@@ -9,39 +9,6 @@ sys.path.append('../2.1')
 from backend import sort
 
 
-"""
-original_json_obj_list = []
-temp_json = ""
-
-for line in sys.stdin:
-    line = line.rstrip()
-    if line != "":
-        
-        temp_json += line
-        try:
-            obj = json.loads(temp_json)
-            original_json_obj_list.append(obj)
-            temp_json = ""
-        except:
-            pass
-num_full_lists = floor(len(original_json_obj_list) / 10)
-split_json_lists = [original_json_obj_list[x:x+10] for x in range(0,num_full_lists*10, 10)]
-# for x in chunks:
-    # print(x)
-list_sorted_lists = [sort(x) for x in split_json_lists]
-
-
-
-# sorted_list = original_json_obj_list
-
-
-
-sorted_list = json.dumps(list_sorted_lists)
-sys.stdout.write(sorted_list)
-"""
-
-
-
 # holds all the objects read from stdin
 original_json_obj_list = []
 
@@ -72,36 +39,3 @@ list_sorted_lists = [sort(x) for x in split_json_lists]
 sorted_list = json.dumps(list_sorted_lists)
 sys.stdout.write(sorted_list)
 
-
-
-"""
-# holds all the objects read from stdin
-original_json_obj_list = []
-
-# holds beginning parts of a json
-temp_json = ""
-
-
-for line in sys.stdin:
-    line = line.rstrip()
-
-    # line_words = line.split()
-
-    for char in line:
-
-        temp_json += char
-        try:
-            obj = json.loads(temp_json)
-            original_json_obj_list.append(obj)
-            temp_json = ""
-        except:
-            pass 
-
-num_full_lists = floor(len(original_json_obj_list) / 10)
-split_json_lists = [original_json_obj_list[x:x+10] for x in range(0,num_full_lists*10, 10)]
-
-list_sorted_lists = [sort(x) for x in split_json_lists]
-
-sorted_list = json.dumps(list_sorted_lists)
-sys.stdout.write(sorted_list)
-"""
