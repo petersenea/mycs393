fc5cea41d679cb2c02302af38f7894c2d7d34dd6
petersenea <Petersenea@sbcglobal.net>
2019-10-08 22:44:29 -0500

    Update test_driver.py

--- Deliverables/2/2.1/test_driver.py
+++ Deliverables/2/2.1/test_driver.py
@@ -14,6 +14,8 @@ decoder = json.JSONDecoder()
 for line in sys.stdin:
     str_json += line
 
+# decodes the first 10 objects, 
+# or all of the objects in the file if there are less than 10
 while len(str_json)>0 or num_objs<10:
     try:
         obj, idx = decoder.raw_decode(str_json)
@@ -24,7 +26,7 @@ while len(str_json)>0 or num_objs<10:
         str_json = str_json[1:]
     
 
-
+# checks to make sure there are exactly 10 objects
 if num_objs == 10:
     sorted_list = sort(special_json_list)
     sorted_list = json.dumps(sorted_list)
